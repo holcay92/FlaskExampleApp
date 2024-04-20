6a9b1d5a3a06122db5e02139a669b67aa798815c
Halil Olcay <holcay92@gmail.com>
2024-04-20 22:17:09 +0300

    database info is fetched

--- app.py
+++ app.py
@@ -1,23 +1,29 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-
 app = Flask(__name__)
-#                                        dialect://username:password@host:port/database
+# Configure the database URI            database://username:password@host:port/database_name
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/example'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Person(db.Model):
-  __tablename__ = 'persons'
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(), nullable=False)
+    __tablename__ = 'persons'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+
+# Define a Flask CLI command to create database tables
+@app.cli.command()
+def create_tables():
+    with app.app_context():
+        db.create_all()
+        print('Database tables created')
+
+@app.route('/')
+def index():
+    person =Person.query.first()
+    return 'Hello ' + person.name
 
 if __name__ == '__main__':
-    with app.app_context():
-        # Inside the app context, create the database tables
-        db.create_all()
-@app.route('/')
-
-def index():
-  return 'Hello World!'
+  
+    app.run(debug=True)
