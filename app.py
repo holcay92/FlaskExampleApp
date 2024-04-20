67469f49c4acd9f6b7317316df8d58f7e0fe4297
Halil Olcay <holcay92@gmail.com>
2024-04-20 21:11:46 +0300

    initial commit

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+#                                        dialect://username:password@host:port/database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/example'
+db = SQLAlchemy(app)
+
+class Person(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+
+
 
 @app.route('/')
 def index():
